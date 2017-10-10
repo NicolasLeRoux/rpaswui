dc44974da62a60a1c6ebf9322cfde7c210f1c693
NicolasLeRoux <mr.leroux.nicolas@gmail.com>
2017-10-10 08:58:54 +0200

    Copy of opencv poc in order to valid the import.

---
+++ index.js
@@ -0,0 +1,26 @@
+var fs = require('fs'),
+	opencv = require('opencv');
+
+var camera = new opencv.VideoCapture(0),
+	window = new opencv.NamedWindow('Video', 0),
+	patternPath = './node_modules/opencv/data/haarcascade_frontalface_alt2.xml';
+
+setInterval(function() {
+	camera.read(function(err, im) {
+		if (err) throw err;
+		console.log(im.size());
+		if (im.size()[0] > 0 && im.size()[1] > 0){
+			im.detectObject(patternPath, {}, function(err, faces) {
+				if (err) throw err;
+
+				for (var i = 0; i < faces.length; i++) {
+					var face = faces[i];
+					im.rectangle([face.x, face.y], [face.width, face.height], [0, 255, 0], 2);
+				}
+
+				window.show(im);
+			});
+		}
+		window.blockingWaitKey(0, 50);
+	});
+}, 20);
