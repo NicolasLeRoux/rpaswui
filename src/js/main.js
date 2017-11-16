314571f910ada51e481dbbe94ce7a7e2b07ad444
NicolasLeRoux <mr.leroux.nicolas@gmail.com>
2017-11-16 09:33:34 +0100

    Adding on orientation change event.

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,11 @@ import m from 'mithril';
 import test from './test.js';
 import style from './main.scss';
 import HeaderComponent from './header/HeaderComponent.js';
+import MainComponent from './main/MainComponent.js';
 
 window.onload = function () {
 	m.route.prefix('');
 
 	m.mount(document.querySelector('body > header'), HeaderComponent);
+	m.mount(document.querySelector('body > main'), MainComponent);
 };
