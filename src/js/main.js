ea66c0bc7f2b1918d208c3eef05d7a7071ae2818
NicolasLeRoux <mr.leroux.nicolas@gmail.com>
2017-11-28 09:18:46 +0100

    feat(Observable): POC sur les proxys et les MutationObservers

--- src/js/main.js
+++ src/js/main.js
@@ -59,3 +59,46 @@ collection.push({
 	id: 'drone003',
 	name: 'Drone'
 });
+
+
+/**
+ * Le proxy pour observer un modèle...
+ * Note: Pas d'aglomération des events. Exemple de la méthode ``sort`` qui va
+ * faire plusieurs appel aux setters... Le ``Proxy`` est un object bas niveau...
+ */
+var array = new Proxy([], {
+	set (target, prop, value) {
+		console.log(target, prop, value);
+		target[prop] = value;
+		return true;
+	}
+});
+window.array = array;
+
+
+/**
+ * Avec les ``MutationObserver``...
+ * Pas de tri sur l'objet ``HTMLCollection``... Par contre, lors de l'ajout de
+ * plusieurs éléments, un seul record est envoyé. Même lors d'ajout à la chaine,
+ * le callback est appellé une fois avec plusieurs mutations.
+ */
+var observer = new MutationObserver(function(mutations) {
+	mutations.forEach(function(mutation) {
+		console.log(mutation);
+	});
+});
+var target = document.createElement('ul');
+window.target = target;
+
+// configuration of the observer:
+var config = {
+	attributes: true,
+	childList: true,
+	characterData: true
+};
+
+// pass in the target node, as well as the observer options
+observer.observe(target, config);
+
+target.append(document.createElement('li'), document.createElement('li'));
+target.append(document.createElement('li'));
