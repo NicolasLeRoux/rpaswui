b4ea4cf8bf9aedfaaaaa0f945b0fa843e0c0a982
Nicolas Le Roux <mr.leroux.nicolas@gmail.com>
2018-01-04 20:29:52 +0100

    feat(Mutation Observer): Delete maladjusted code

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 import test from './test.js';
 import PersonneModel from './PersonneModel.js';
 import ObservableCollection from './ObservableCollection.js';
-import MutationObserverModel from './MutationObserverModel.js';
 
 // Liste de drone disponible au pilotage.
 var drones = [
@@ -75,51 +74,3 @@ var array = new Proxy([], {
 	}
 });
 window.array = array;
-
-
-/**
- * Avec les ``MutationObserver``...
- * Pas de tri sur l'objet ``HTMLCollection``... Par contre, lors de l'ajout de
- * plusieurs éléments, un seul record est envoyé. Même lors d'ajout à la chaine,
- * le callback est appellé une fois avec plusieurs mutations.
- */
-var observer = new MutationObserver(function(mutations) {
-	mutations.forEach(function(mutation) {
-		console.log(mutation);
-	});
-});
-var target = document.createElement('ul');
-window.target = target;
-
-// configuration of the observer:
-var config = {
-	attributes: true,
-	childList: true,
-	characterData: true
-};
-
-// pass in the target node, as well as the observer options
-observer.observe(target, config);
-
-target.append(document.createElement('li'), document.createElement('li'));
-target.append(document.createElement('li'));
-
-
-/**
- * Warning, impossible d'avoir des majuscule dans les noms des attributes...
- */
-class Pers extends MutationObserverModel {
-	static get observedAttributes() {
-		return [
-			'name',
-			'age'
-		]
-	}
-
-	constructor (attrs) {
-		super(attrs);
-	}
-}
-
-var pers = new Pers();
-window.pers = pers;
