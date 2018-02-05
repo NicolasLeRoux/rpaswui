323369234b00cbf7aa91909a66795b60d659641b
Nicolas Le Roux <mr.leroux.nicolas@gmail.com>
2018-02-05 22:59:14 +0100

    style(Indent): Switch indent from space to tab

--- src/js/components/rpas-socket.component.js
+++ src/js/components/rpas-socket.component.js
@@ -1,64 +1,64 @@
 export class SocketComponent extends HTMLElement {
-    static get name () {
-        return 'rpas-socket';
-    }
+	static get name () {
+		return 'rpas-socket';
+	}
 
-    constructor () {
-        super();
-    }
+	constructor () {
+		super();
+	}
 
-    /**
-     * Invoked when the custom element is first connected to the document's DOM.
-     */
-    connectedCallback () {
-        this.ws.onopen = this.onOpenSocket.bind(this);
-        this.ws.onmessage = this.onMessageSocket.bind(this);
-    }
+	/**
+	 * Invoked when the custom element is first connected to the document's DOM.
+	 */
+	connectedCallback () {
+		this.ws.onopen = this.onOpenSocket.bind(this);
+		this.ws.onmessage = this.onMessageSocket.bind(this);
+	}
 
-    onOpenSocket () {
-        this.ws.send(JSON.stringify({
-            type: 'PILOT',
-            action: 'INIT_SOCKET'
-        }));
+	onOpenSocket () {
+		this.ws.send(JSON.stringify({
+			type: 'PILOT',
+			action: 'INIT_SOCKET'
+		}));
 
-        // TODO tmp
-        this.data = {
-            name: 'Pierre'
-        };
-        var evt = new Event('look');
-        this.dispatchEvent(evt);
-    }
+		// TODO tmp
+		this.data = {
+			name: 'Pierre'
+		};
+		var evt = new Event('look');
+		this.dispatchEvent(evt);
+	}
 
-    onMessageSocket () {
-    }
+	onMessageSocket () {
+	}
 
-    /**
-     * Invoked when the custom element is moved to a new document.
-     */
-    adoptedCallback () {
-        console.log('adoptedCallback');
-    }
+	/**
+	 * Invoked when the custom element is moved to a new document.
+	 */
+	adoptedCallback () {
+		console.log('adoptedCallback');
+	}
 
-    /**
-     * Invoked when the custom element is disconnected from the document's DOM.
-     */
-    disconnectedCallback () {
-    }
+	/**
+	 * Invoked when the custom element is disconnected from the document's DOM.
+	 */
+	disconnectedCallback () {
+	}
 
-    get ws () {
-        if (!this._ws) {
-            this._ws = new WebSocket('ws://localhost:3000', 'echo-protocol');
-        }
-        return this._ws;
-    }
+	get ws () {
+		if (!this._ws) {
+			this._ws = new WebSocket('ws://localhost:3000', 'echo-protocol');
+		}
+		return this._ws;
+	}
 
-    get data () {
-        return this._data;
-    }
+	get data () {
+		return this._data;
+	}
 
-    set data (data) {
-        this._data = data;
-    }
+	set data (data) {
+		this._data = data;
+	}
 };
 
 customElements.define(SocketComponent.name, SocketComponent);
