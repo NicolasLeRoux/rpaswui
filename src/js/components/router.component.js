ae32bd21e2f87c0b98368512393f38a5dfc69340
Nicolas Le Roux <mr.leroux.nicolas@gmail.com>
2018-02-05 23:35:51 +0100

    feat(Router): Create 2 routes to navigate

--- src/js/components/router.component.js
+++ src/js/components/router.component.js
@@ -3,8 +3,41 @@ export class RouterComponent extends HTMLElement {
 		return 'rpas-router';
 	}
 
+	static get observedAttributes () {
+		return [
+			'data-route'
+		];
+	}
+
 	constructor () {
 		super();
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		switch (name) {
+			case 'data-route':
+				this.onRouteChange(name, oldValue, newValue);
+				break;
+			default:
+				console.warn('Unwatch attribute', name);
+		}
+	}
+
+	onRouteChange (name, oldValue, newValue) {
+		let newElm = this.template.querySelector(`[data-route="${newValue}"]`),
+			oldElm = this.querySelector(`[data-route="${oldValue}"]`);
+
+		if (oldElm) {
+			oldElm.remove();
+		}
+
+		if (newElm) {
+			this.append(document.importNode(newElm, true));
+		}
+	}
+
+	get template () {
+		return this.children[0].content;
 	}
 };
 
