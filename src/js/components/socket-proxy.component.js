f550a26a911ef2dbcef2eddbe280c238bb5203a0
NicolasLeRoux <mr.leroux.nicolas@gmail.com>
2018-02-07 13:48:11 +0100

    feat(Proxy): Add socket proxy to wrap event with the recipient

---
+++ src/js/components/socket-proxy.component.js
@@ -0,0 +1,33 @@
+export class SocketProxyComponent extends HTMLElement {
+	static get name () {
+		return 'rpas-socket-proxy';
+	}
+
+	constructor () {
+		super();
+	}
+
+	connectedCallback () {
+        this.querySelector('rpas-socket')
+            .addEventListener('message', this.onMessage.bind(this));
+	}
+
+    onMessage (event) {
+        let data = event.detail;
+
+        if (data.action === 'UPDATE_REMOTE') {
+            let newData = Object.assign({}, data, {
+                    recipient: 'mon-destinataire'
+                }),
+			    evt = new CustomEvent('message', {
+                    bubbles: true,
+                    detail: newData
+                });
+
+            event.stopPropagation();
+            this.dispatchEvent(evt);
+        }
+    }
+};
+
+customElements.define(SocketProxyComponent.name, SocketProxyComponent);
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import { SocketComponent } from './components/socket.component.js';
+import { SocketProxyComponent } from './components/socket-proxy.component.js';
 import { RouterComponent } from './components/router.component.js';
 import { MessageMediatorComponent } from './components/message-mediator.component.js';
 
