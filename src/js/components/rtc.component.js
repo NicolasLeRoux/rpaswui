46025cefd4270d54ade6d08849c7c36e6215f28c
Nicolas Le Roux <mr.leroux.nicolas@gmail.com>
2018-02-25 17:10:03 +0100

    feat(Command Channel): Delete init message

--- src/js/components/rtc.component.js
+++ src/js/components/rtc.component.js
@@ -48,7 +48,7 @@ export class RTCComponent extends HTMLElement {
 
 		commandChannel.onopen = function () {
 			console.info('Command channel opened.');
-			commandChannel.send('Client: Mon super message !');
+
 			document.querySelectorAll('[data-command-direction]').forEach((item) => {
 				item.onclick = () => {
 					commandChannel.send(JSON.stringify({
