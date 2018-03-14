256acf79c0603e6973647bbc28640d98e723b6b9
Nicolas Le Roux <mr.leroux.nicolas@gmail.com>
2018-03-14 21:21:32 +0100

    feat(Fullscreen Icon): Add shadow DOM with fullscreen SVG

--- src/js/components/full-screen-icon.component.js
+++ src/js/components/full-screen-icon.component.js
@@ -1,10 +1,18 @@
-const tmpl = `
-	<svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false">
-		<g>
-			<path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"></path>
-		</g>
-	</svg>
-`;
+const tmpl = function (data = {}) {
+	return `
+		<style>
+			svg {
+				width: 24px;
+				height: 24px;
+			}
+		</style>
+		<svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false">
+			<g>
+				<path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"></path>
+			</g>
+		</svg>
+	`;
+};
 
 export class FullScreenIconComponent extends HTMLElement {
 
@@ -17,6 +25,15 @@ export class FullScreenIconComponent extends HTMLElement {
 	}
 
 	connectedCallback () {
+		const shadow = this.attachShadow({
+				mode: 'open'
+			});
+		const tmpElm = document.createElement('div');
+		tmpElm.innerHTML = tmpl();
+
+		[...tmpElm.children].forEach((child) => {
+			shadow.appendChild(child);
+		});
 	}
 }
 
