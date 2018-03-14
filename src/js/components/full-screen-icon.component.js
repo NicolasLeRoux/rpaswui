1894f4165d8a3fcdea377f81146abea96a05eca0
Nicolas Le Roux <mr.leroux.nicolas@gmail.com>
2018-03-14 20:44:28 +0100

    style(Indent): Switch indent from space to tabs

--- src/js/components/full-screen-icon.component.js
+++ src/js/components/full-screen-icon.component.js
@@ -1,23 +1,23 @@
 const tmpl = `
-    <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false">
-        <g>
-            <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"></path>
-        </g>
-    </svg>
+	<svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false">
+		<g>
+			<path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"></path>
+		</g>
+	</svg>
 `;
 
 export class FullScreenIconComponent extends HTMLElement {
 
-    static get name () {
-        return 'rpas-full-screen-icon';
-    }
+	static get name () {
+		return 'rpas-full-screen-icon';
+	}
 
-    constructor () {
-        super();
-    }
+	constructor () {
+		super();
+	}
 
 	connectedCallback () {
-    }
+	}
 }
 
 customElements.define(FullScreenIconComponent.name, FullScreenIconComponent);
--- src/js/components/full-screen.component.js
+++ src/js/components/full-screen.component.js
@@ -1,29 +1,29 @@
 import fs from '../vendors/fullscreen.js';
 
 export class FullScreenComponent extends HTMLElement {
-    static get name () {
-        return 'rpas-full-screen';
-    }
+	static get name () {
+		return 'rpas-full-screen';
+	}
 
-    constructor () {
-        super();
-    }
+	constructor () {
+		super();
+	}
 
 	connectedCallback () {
-        const goElm = this.querySelector('button.go-fs');
-        const exitElm = this.querySelector('button.exit-fs');
+		const goElm = this.querySelector('button.go-fs');
+		const exitElm = this.querySelector('button.exit-fs');
 
-        goElm.addEventListener('click', this.onClickGo.bind(this));
-        exitElm.addEventListener('click', this.onClickExit.bind(this));
-    }
+		goElm.addEventListener('click', this.onClickGo.bind(this));
+		exitElm.addEventListener('click', this.onClickExit.bind(this));
+	}
 
-    onClickGo () {
-        fs.requestFullscreen(this);
-    }
+	onClickGo () {
+		fs.requestFullscreen(this);
+	}
 
-    onClickExit () {
-        fs.exitFullscreen();
-    }
+	onClickExit () {
+		fs.exitFullscreen();
+	}
 }
 
 customElements.define(FullScreenComponent.name, FullScreenComponent);
